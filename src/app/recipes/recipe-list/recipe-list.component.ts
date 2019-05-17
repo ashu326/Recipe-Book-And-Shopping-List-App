5b7a5fde2c018e5ae377d3cb21c0431a6a379886
Ashutosh Chauhan <ashutoshchauhan326@gmail.com>
2019-05-17 16:11:05 +0530

    added new features to components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Burger', 'burger looks like a burger', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLvKZ2wB2zh83m3y3vmqEiMNgajr5mrTbJ6Vv3HZgFccQiMFCS'),
+    new Recipe('Burger', 'burger looks like a burger', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLvKZ2wB2zh83m3y3vmqEiMNgajr5mrTbJ6Vv3HZgFccQiMFCS')
+
+  ];
 
   constructor() { }
 
